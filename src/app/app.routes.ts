3b45d657f7a37af90615c7326191cbe08e29001b
Camilo Rivera <camilo2986@gmail.com>
2025-11-27 15:20:27 -0500

    Configuración de navegación
    Se congigura menú de navegación en el layout, rutas y botones

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,28 +3,25 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./components/layout/layout').then((m) => m.Layout),
+    loadComponent: () => import('./components/layout/layout').then((m) => m.Layout),    
     children: [
       {
         path: 'introduccion',
-        loadComponent: () => import('./features/introduccion/introduccion').then((m) => m.Introduccion),       
-
+        loadComponent: () =>
+          import('./features/introduccion/introduccion').then((m) => m.Introduccion),
       },
       {
         path: 'objetivos',
-        loadComponent: () => import('./features/objetivos/objetivos').then((m) => m.Objetivos),       
-
+        loadComponent: () => import('./features/objetivos/objetivos').then((m) => m.Objetivos),
       },
       {
         path: 'contenido',
-        loadComponent: () => import('./features/contenido/contenido').then((m) => m.Contenido),       
-
+        loadComponent: () => import('./features/contenido/contenido').then((m) => m.Contenido),
       },
       {
         path: 'actividad',
-        loadComponent: () => import('./features/actividad/actividad').then((m) => m.Actividad),       
-
-      }
-    ]
+        loadComponent: () => import('./features/actividad/actividad').then((m) => m.Actividad),
+      },
+    ],
   },
 ];
--- src/app/components/layout/layout.ts
+++ src/app/components/layout/layout.ts
@@ -1,12 +1,54 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLinkWithHref, RouterLinkActive, Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, RouterLinkWithHref, RouterLinkActive],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
 export class Layout {
+  
+  _menuItems = [
+    { label: 'Introducción', path: 'introduccion'},
+    { label: 'Objetivos', path: 'objetivos'},
+    { label: 'Contenido', path: 'contenido'},
+    { label: 'Actividad', path: 'actividad'},
+  ]
+  _currentPath = location.pathname;
+  _prevAvailable = false;
+  _nextAvailable = false;
 
+  constructor(
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(() => {
+      this._currentPath = location.pathname;      
+      const currentIndex = this.getIndexPath(this._currentPath);
+      this._prevAvailable = currentIndex > 0;
+      this._nextAvailable = currentIndex < this._menuItems.length - 1;
+    });
+  }
+
+  getIndexPath(path: string): number {
+    return this._menuItems.findIndex(item => item.path === path.replace('/',''));
+  }
+
+  goPrevious() {
+    const currentIndex = this.getIndexPath(this._currentPath);
+    if (currentIndex > 0) {
+      const previousItem = this._menuItems[currentIndex - 1];
+      this.router.navigate([previousItem.path]);
+    }
+  }
+  goNext() {
+    const currentIndex = this.getIndexPath(this._currentPath);
+    if (currentIndex < this._menuItems.length - 1) {
+      const nextItem = this._menuItems[currentIndex + 1];
+      this.router.navigate([nextItem.path]);
+    }
+  }
 }
