71e906c6d43a43db696a736f48fec733fde7728b
Camilo Rivera <camilo2986@gmail.com>
2025-11-27 14:40:17 -0500

    Configuración de Rutas, Bootstrap y Layout
    Se utiliza Bootstrap como  framework de estilos simple y rápida de implementar.
    
    Se configura un Layout separado para descentralizar.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./components/layout/layout').then((m) => m.Layout),
+    children: [
+      {
+        path: 'introduccion',
+        loadComponent: () => import('./features/introduccion/introduccion').then((m) => m.Introduccion),       
+
+      },
+      {
+        path: 'objetivos',
+        loadComponent: () => import('./features/objetivos/objetivos').then((m) => m.Objetivos),       
+
+      },
+      {
+        path: 'contenido',
+        loadComponent: () => import('./features/contenido/contenido').then((m) => m.Contenido),       
+
+      },
+      {
+        path: 'actividad',
+        loadComponent: () => import('./features/actividad/actividad').then((m) => m.Actividad),       
+
+      }
+    ]
+  },
+];
